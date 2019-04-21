75f31af166f31fec341e9db879d3b1920daa8a48
xuezzou <ilfam1999@gmail.com>
2019-04-21 11:50:02 -0500

    add two stories lines

--- conversation.js
+++ conversation.js
@@ -1,140 +1,241 @@
-var chatMessages = [{
-    name: "ms1",
-    msg: "I really like you, Midori. A lot.",
-    delay: 500,
-    align: "right",
-    showTime: true,
-    time: "19:58"
-  },
-  {
-    name: "ms2",
-    msg: "How much is a lot?",
-    delay: 500,
-    align: "left",
-    showTime: true,
-    time: "19:58"
-  },
-  {
-    name: "ms3",
-    msg: "Like a spring bear,",
-    delay: 500,
-    align: "right",
-    showTime: true,
-    time: "19:58"
-  },
-  {
-    name: "ms4",
-    msg: "A spring bear?",
-    delay: 500,
-    align: "left",
-    showTime: false,
-    time: "19:58"
-  },
-  {
-    name: "ms5",
-    msg: "What’s that all about? A spring bear.",
-    delay: 1000,
-    align: "left",
-    showTime: true,
-    time: "19:58"
-  },
-  {
-    name: "ms6",
-    msg: "You’re walking through a field all by yourself one day in spring,",
-    delay: 1000,
-    align: "right",
-    showTime: false,
-    time: "19:58"
-  },
-  {
-    name: "ms7",
-    msg: "and this sweet little bear cub with velvet fur and shiny little eyes comes walking along.",
-    delay: 2000,
-    align: "right",
-    showTime: false,
-    time: "19:58"
-  },
-  {
-    name: "ms8",
-    msg: "And he says to you,",
-    delay: 2000,
-    align: "right",
-    showTime: false,
-    time: "19:58"
-  },
-  {
-    name: "ms9",
-    msg: "\"Hi, there, little lady. Want to tumble with me?\"",
-    delay: 5000,
-    align: "right",
-    showTime: false,
-    time: "19:58"
-  },
-  {
-    name: "ms10",
-    msg: "So you and the bear cub spend the whole day in each other’s arms,",
-    delay: 2000,
-    align: "right",
-    showTime: false,
-    time: "19:58"
-  },
-  {
-    name: "ms11",
-    msg: "tumbling down this clover-covered hill.",
-    delay: 2000,
-    align: "right",
-    showTime: false,
-    time: "19:58"
-  },
-  {
-    name: "ms12",
-    msg: "Nice, huh?",
-    delay: 1000,
-    align: "right",
-    showTime: true,
-    time: "19:58"
-  },
-  {
-    name: "ms14",
-    msg: "Yeah. Really nice",
-    delay: 2000,
-    align: "left",
-    showTime: true,
-    time: "19:58"
-  },
-  {
-    name: "ms15",
-    msg: "That’s how much I like you.",
-    delay: 3000,
-    align: "right",
-    showTime: true,
-    time: "19:58"
-  }
-];
-  var chatDelay = 0;
-  
-  function onRowAdded() {
-    $('.chat-container').animate({
-      scrollTop: $('.chat-container').prop('scrollHeight')
-    });
-  };
-  $.each(chatMessages, function(index, obj) {
-    chatDelay = chatDelay + 4000;
-    chatDelay2 = chatDelay + obj.delay;
-    chatDelay3 = chatDelay2 + 10;
-    scrollDelay = chatDelay;
-    chatTimeString = " ";
-    msgname = "." + obj.name;
-    msginner = ".messageinner-" + obj.name;
-    spinner = ".sp-" + obj.name;
+// messages
+let greeting = [{
+  name: "last",
+  msg: "Nice to meet you :-D",
+  delay: 500,
+  align: "left",
+  showTime: true,
+  time: "19:58"
+}];
+
+let choiceWhoAmI = [{
+  name: "ms1",
+  msg: "Who are you",
+  delay: 500,
+  align: "right",
+  showTime: true,
+  time: "19:58"
+}];
+
+let whoAmI = [{
+  name: "ms1",
+  msg: "A piano player who can't write good code and good poetry",
+  delay: 500,
+  align: "left",
+  showTime: false,
+  time: "19:58"
+}, {
+  name: "githubLink",
+  msg: "Here's my <a target='_blank' href='https://github.com/xuezzou'>Github</a>",
+  delay: 500,
+  align: "left",
+  showTime: false,
+  time: "19:58"
+}, {
+  name: "last",
+  msg: "And my <a target='_blank' href='xue_zou_resume_Dec18.pdf'>resume</a> if you are interested <br/> (๑¯◡¯๑)",
+  delay: 500,
+  align: "left",
+  showTime: true,
+  time: "19:58"
+}];
+
+let choiceTellAStory = [{
+  name: "ms1",
+  msg: "Tell a story",
+  delay: 500,
+  align: "right",
+  showTime: true,
+  time: "19:58"
+}];
+
+let storyNorwegianWood = [{
+  name: "ms1",
+  msg: "...",
+  delay: 500,
+  align: "left",
+  showTime: true,
+  time: "19:58"
+}, {
+  name: "ms1",
+  msg: "I really like you, Midori. A lot.",
+  delay: 500,
+  align: "right",
+  showTime: true,
+  time: "19:58"
+}, {
+  name: "ms2",
+  msg: "How much is a lot?",
+  delay: 500,
+  align: "left",
+  showTime: true,
+  time: "19:58"
+}, {
+  name: "ms3",
+  msg: "Like a spring bear,",
+  delay: 500,
+  align: "right",
+  showTime: true,
+  time: "19:58"
+}, {
+  name: "ms4",
+  msg: "A spring bear?",
+  delay: 500,
+  align: "left",
+  showTime: false,
+  time: "19:58"
+}, {
+  name: "ms5",
+  msg: "What’s that all about? A spring bear.",
+  delay: 1000,
+  align: "left",
+  showTime: true,
+  time: "19:58"
+}, {
+  name: "ms6",
+  msg: "You’re walking through a field all by yourself one day in spring,",
+  delay: 1000,
+  align: "right",
+  showTime: false,
+  time: "19:58"
+}, {
+  name: "ms7",
+  msg: "and this sweet little bear cub with velvet fur and shiny little eyes comes walking along.",
+  delay: 2000,
+  align: "right",
+  showTime: false,
+  time: "19:58"
+}, {
+  name: "ms8",
+  msg: "And he says to you,",
+  delay: 2000,
+  align: "right",
+  showTime: false,
+  time: "19:58"
+}, {
+  name: "ms9",
+  msg: "\"Hi, there, little lady. Want to tumble with me?\"",
+  delay: 5000,
+  align: "right",
+  showTime: false,
+  time: "19:58"
+}, {
+  name: "ms10",
+  msg: "So you and the bear cub spend the whole day in each other’s arms,",
+  delay: 2000,
+  align: "right",
+  showTime: false,
+  time: "19:58"
+}, {
+  name: "ms11",
+  msg: "tumbling down this clover-covered hill.",
+  delay: 2000,
+  align: "right",
+  showTime: false,
+  time: "19:58"
+}, {
+  name: "ms12",
+  msg: "Nice, huh?",
+  delay: 1000,
+  align: "right",
+  showTime: true,
+  time: "19:58"
+}, {
+  name: "ms14",
+  msg: "Yeah. Really nice",
+  delay: 2000,
+  align: "left",
+  showTime: true,
+  time: "19:58"
+}, {
+  name: "last",
+  msg: "That’s how much I like you.",
+  delay: 3000,
+  align: "right",
+  showTime: true,
+  time: "19:58"
+}];
+
+
+// loading the 
+
+
+function onRowAdded() {
+  $('.chat-container').animate({
+    scrollTop: $('.chat-container').prop('scrollHeight')
+  });
+};
+let choieOption = 0; // a global variable that controls the flow 
+let chatDelay = 0; // chatDelay var that controls the time of display of messages
+
+function displayMessages(chatArea, chatMessages) {
+  $.each(chatMessages, function (index, obj) {
+    // chatDelay = chatDelay + 2000; // control the gerneral delayed time
+    let chatDelay2 = chatDelay + obj.delay;
+    let chatDelay3 = chatDelay2 + 10;
+    // let scrollDelay = chatDelay;
+    let chatTimeString = " ";
+    let msgname = "." + obj.name;
+    let msginner = ".messageinner-" + obj.name;
+    let spinner = ".sp-" + obj.name;
     if (obj.showTime == true) {
       chatTimeString = "<span class='message-time'>" + obj.time + "</span>";
     }
-    $(".chat-message-list").append("<li class='message-" + obj.align + " " + obj.name + "' hidden><div class='sp-" + obj.name + "'><span class='spinme-" + obj.align + "'><div class='spinner'><div class='bounce1'></div><div class='bounce2'></div><div class='bounce3'></div></div></span></div><div class='messageinner-" + obj.name + "' hidden><span class='message-text'>" + obj.msg + "</span>" + chatTimeString + "</div></li>");
+
+    $(chatArea).append("<li class='message-" + obj.align + " " + obj.name + "' hidden><div class='sp-" + obj.name + "'><span class='spinme-" + obj.align + "'><div class='spinner'><div class='bounce1'></div><div class='bounce2'></div><div class='bounce3'></div></div></span></div><div class='messageinner-" + obj.name + "' hidden><span class='message-text'>" + obj.msg + "</span>" + chatTimeString + "</div></li>");
     $(msgname).delay(chatDelay).fadeIn();
     $(spinner).delay(chatDelay2).hide(1);
     $(msginner).delay(chatDelay3).fadeIn();
     setTimeout(onRowAdded, chatDelay);
     setTimeout(onRowAdded, chatDelay3);
     chatDelay = chatDelay3;
+
+    if (index === chatMessages.length - 1 && obj.name === "last") {
+      // choieOption++;
+      $(".user-message").delay(chatDelay3 + 1000).queue(function (n) {
+        if (choieOption % 2 === 0) {
+          $(this).html(choiceWhoAmI[0].msg);
+        } else if (choieOption % 2 === 1) {
+          $(this).html(choiceTellAStory[0].msg);
+        }
+        choieOption = choieOption + 1;
+        
+
+        n();
+      });
+      // $(".userMessage").delay(chatDelay3 + 1000)
+      console.log(choieOption);
+      console.log('last one' + chatDelay3);
+      //chatDelay3 + 1000
+    }
   });
+}
+
+function displayUserOption(chatMessages) {
+  // $(".user-chat").append("<li>hi</li>");
+  // displayMessages()
+};
+
+function loadingConversation() {
+  displayMessages(".chat-message-list", greeting);
+  // displayMessages(".user-chat", choiceWhoAmI);
+};
+
+$('.user-message').on("click", function () {
+  chatDelay = 0;
+  if (this.innerText === choiceWhoAmI[0].msg) {
+    displayMessages(".chat-message-list", choiceWhoAmI);
+    displayMessages(".chat-message-list", whoAmI);
+  } else if (this.innerText === choiceTellAStory[0].msg) {
+    displayMessages(".chat-message-list", choiceTellAStory);
+    displayMessages(".chat-message-list", storyNorwegianWood);
+  } else {
+    // again the conversation
+  }
+  $(this).html("Say something...");
+  // $(this).off('hover');
+
+
+});
+
+loadingConversation();
